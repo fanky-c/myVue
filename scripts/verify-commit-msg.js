9465b49468fe987be69182a7db91879a7f1357d2
zhaochao <zhaochao@rd.netease.com>
2020-05-04 19:56:45 +0800

    docs: 生成commit-msg信息

--- scripts/verify-commit-msg.js
+++ scripts/verify-commit-msg.js
@@ -8,11 +8,11 @@ const commitRE = /^(revert: )?(feat|fix|polish|docs|style|refactor|perf|test|wor
 if (!commitRE.test(msg)) {
   console.log()
   console.error(
-    `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(`invalid commit message format.`)}\n\n` +
-    chalk.red(`  Proper commit message format is required for automated changelog generation. Examples:\n\n`) +
+    `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(`不合法的提交信息格式.`)}\n\n` +
+    chalk.red(`  自动生成变更日志需要正确的提交消息格式。例如:\n\n`) +
     `    ${chalk.green(`feat(compiler): add 'comments' option`)}\n` +
     `    ${chalk.green(`fix(v-model): handle events on blur (close #28)`)}\n\n` +
-    chalk.red(`  You can also use ${chalk.cyan(`npm run commit`)} to interactively generate a commit message.\n`)
+    chalk.red(`  你也可以使用命令行 ${chalk.cyan(`npm run commit`)} 来交互式生成提交消息.\n`)
   )
   process.exit(1)
 }
